bf06833d4cfeb7224326bf77c66f042196061536
Elton Kok <elton.kok@mail.utoronto.ca>
2019-04-13 13:23:32 -0400

    Update to conform with pylint

--- d01_scrape.py
+++ d01_scrape.py
@@ -1,24 +1,26 @@
+import urllib
 from bs4 import BeautifulSoup
 import requests
-import urllib
 
 # a script used to scrape pdfs off the following website
-url = r'https://www.utsc.utoronto.ca/~atafliovich/cscd01/project.html'
-base_url = r'https://www.utsc.utoronto.ca/~atafliovich/cscd01/'
-source = requests.get(url).text
+URL = r'https://www.utsc.utoronto.ca/~atafliovich/cscd01/lectures.html'
+BASE_URL = r'https://www.utsc.utoronto.ca/~atafliovich/cscd01/'
+SOURCE = requests.get(URL).text
 
-soup = BeautifulSoup(source, 'lxml')
+SOUP = BeautifulSoup(SOURCE, 'lxml')
 
 
 def main():
-    # finds all the lecture pdfs in the table and downloads to current directory
-    table = soup.find('table')
+    '''
+    Finds all the lecture pdfs in the table and downloads to current directory
+    '''
+    table = SOUP.find('table')
     table_anchors = table.find_all('a')
 
     for i in range(len(table_anchors)):
         cur_file = table_anchors[i]['href']
         file_name = cur_file.split('/')[-1]
-        urllib.request.urlretrieve(base_url+cur_file, file_name)
+        urllib.request.urlretrieve(BASE_URL+cur_file, file_name)
         print("Successfully downloaded: " + cur_file + " and saved as: " + file_name)
 
 if __name__ == "__main__":
