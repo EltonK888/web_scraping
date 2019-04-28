61ff071d87232b417daca2f101464859728e81b0
Elton Kok <elton.kok@mail.utoronto.ca>
2019-04-28 17:34:54 -0400

    Add script to scrape CSCB63

---
+++ b63_scrape.py
@@ -0,0 +1,25 @@
+import urllib
+import requests
+from bs4 import BeautifulSoup
+
+# a script used to scrape pdfs off the following website
+URL = r'http://www.utsc.utoronto.ca/~bretscher/b63/lectures.html'
+SOURCE = requests.get(URL).text
+SOUP = BeautifulSoup(SOURCE, 'lxml')
+
+
+def main():
+    '''
+    Downloads all lecture pdfs into the working directory
+    from the website specified by URL
+    '''
+    table = SOUP.find('table')
+    table_anchors = table.find_all('a')
+    for i in range(len(table_anchors)):
+        cur_file = table_anchors[i]['href']
+        file_name = cur_file.split('/')[-1]
+        urllib.request.urlretrieve(cur_file, file_name)
+        print("Successfully downloaded: " + cur_file + " and saved as: " + file_name)
+
+if __name__ == "__main__":
+    main()
